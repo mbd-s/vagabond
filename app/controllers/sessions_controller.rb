c6b8e3ab0864ae2e0680b282fb3adbb37e5c280c
Hila Vasiler <hila.marmel@gmail.com>
2016-05-05 10:53:13 -0700

    comleted login, logout, signup functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,26 @@
 class SessionsController < ApplicationController
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    #this method is called when the user logs in, it looks up the user's record
+    #in †he db and validates that the password matches
+    user_params = params.require(:user).permit(:email, :password)
+    #confrim is an instance method of user (in the user model)
+    @user = User.confirm(user_params)
+    if @user
+      login(@user) # <-- login the user
+      redirect_to @user # <-- go to show
+    else
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    logout # this method lives in the SessionsHelper!
+    redirect_to root_path
+  end
+
 end
