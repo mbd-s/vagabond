db06a9ee89ca5f3969a9cd74545b3716bc10e035
Sam <samuel1008@gmail.com>
2016-05-05 13:37:38 -0700

    removed user_id column from posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160505003542) do
+ActiveRecord::Schema.define(version: 20160505203229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,10 @@ ActiveRecord::Schema.define(version: 20160505003542) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "post_id"
   end
 
+  add_index "users", ["post_id"], name: "index_users_on_post_id", using: :btree
+
+  add_foreign_key "users", "posts"
 end
