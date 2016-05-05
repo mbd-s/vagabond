c45363fd20de8c8c8c54755a148ae5c2514ac869
lsquach <lsquach@gmail.com>
2016-05-04 21:14:52 -0700

    added show, edit, and update in user controller. added views for user show and edit.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,23 @@ class UsersController < ApplicationController
     login(@user)
     redirect_to @user
   end
+  def edit
+    @user = User.find_by_id(params[:id])
+  end
+  def update
+    @user = User.find_by_id(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
   def show
     @user = User.find_by_id(params[:id])
   end
 
   private
-  
+
   def user_params
     params.require(:user).permit(:email, :first_name, :last_name, :current_city, :image, :password)
   end
