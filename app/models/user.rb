6f2a4d964bf4e678d1d381f375008a9c73dce38d
Sam <samuel1008@gmail.com>
2016-05-05 13:24:51 -0700

    updated associations on user's model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
+  has_many :posts
+
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
     #authenticate is a Bcrypt method
