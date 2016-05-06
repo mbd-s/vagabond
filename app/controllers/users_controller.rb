e7b607ede89871e8b71ae1e7d44425e771cb3360
mbd-s <michaelbsilverberg@gmail.com>
2016-05-06 12:23:56 -0700

    Revert "aborting aws paperclip deploy"
    This reverts commit 3c37c06b268b8b1e222809026aa5266484822de8.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,20 +4,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      login(@user)
-      redirect_to @user
-    else
-      flash[:error]=@user.errors.full_messages
-      redirect_to new_user_path
-    end
+    @user = User.create(user_params)
+    login(@user)
+    redirect_to @user
   end
-
   def edit
     set_user
   end
-
   def update
     set_user
     if @user.update(user_params)
@@ -32,10 +25,6 @@ class UsersController < ApplicationController
     @posts = @user.posts
   end
 
-  def index
-
-  end
-
   # Deleting user is a 'bonus' feature
   # def destroy
   #   @user = User.find_by_id(params[:id]).destroy
@@ -45,7 +34,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :first_name, :last_name, :current_city, :password)
+    params.require(:user).permit(:email, :first_name, :last_name, :current_city, :avatar, :password)
   end
   def set_user
     user_id = params[:id]
