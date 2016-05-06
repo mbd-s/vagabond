41393a069d035b93cb163384e443ed86e15df13d
Sam <samuel1008@gmail.com>
2016-05-06 10:51:07 -0700

    added columns city and post for id

---
+++ db/migrate/20160506174535_add_post_id_to_city.rb
@@ -0,0 +1,5 @@
+class AddPostIdToCity < ActiveRecord::Migration
+  def change
+    add_reference :cities, :post, index: true, foreign_key: true
+  end
+end
---
+++ db/migrate/20160506174740_add_city_id_to_post.rb
@@ -0,0 +1,5 @@
+class AddCityIdToPost < ActiveRecord::Migration
+  def change
+    add_reference :posts, :city, index: true, foreign_key: true
+  end
+end
