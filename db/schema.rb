fbf412593de213bd6c64eb7aa91f8f61ddf385a6
Sam <samuel1008@gmail.com>
2016-05-06 11:06:09 -0700

    fixed migration file

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20160506174740) do
-=======
-ActiveRecord::Schema.define(version: 20160506164720) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
