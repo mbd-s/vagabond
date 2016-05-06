c941e8a09e9277e6bdcb38285556742ee315ec7a
Hila Vasiler <hila.marmel@gmail.com>
2016-05-05 21:16:04 -0700

    added email validation in sign up page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :posts
+  # validates :password,
+  #           confirmation: true,
+  #           length: {minimum: 6}
+  validates :email,
+            presence: true,
+            uniqueness: true
 
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
