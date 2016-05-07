f935acbbaf280b4234e2ecf846a4fd145630bdcf
mbd-s <michaelbsilverberg@gmail.com>
2016-05-06 18:08:08 -0700

    validating posts, still need to test

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :city
+
+  validates :title, length: {
+    minimum: 1,
+    maximum: 255
+  }
+  validates :content, presence: true
 end
