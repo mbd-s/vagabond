a4e0a995d9a1fe4da15010e9536bd26c90275197
Sam <samuel1008@gmail.com>
2016-05-08 12:42:07 -0700

    commiting before merge

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -5,10 +5,19 @@ class CitiesController < ApplicationController
   end
 
   def show
+
+
     if current_user
+      @poster = Post.find(params[:id])
+      writter_id = @poster.user_id
+      @writter = User.find(writter_id)
       city_id = params[:id]
       @city = City.find_by(id: city_id)
       @post = @city.posts
+
+
+
+
       render :show
     else
       redirect_to root_path
