ff9f556b72b46dc88a825a41891dcd7348db1180
Sam <samuel1008@gmail.com>
2016-05-08 14:46:32 -0700

    updated controller to show user's first and last name

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -9,22 +9,7 @@ class CitiesController < ApplicationController
       city_id = params[:id]
       @city = City.find_by(id: city_id)
       @post = @city.posts
-
-      # newer
-      @users = User.all
-      @user = User.find(params[:id])
-      users_post = @user.posts
-      writter_id = users_post.find(params[:id])
-      @writter = User.find(writter_id)
-
-      @poster = Post.find(params[:id])
-      user_id = @post.user_id
-      @author = User.find(user_id)
-      user_city_id = @author.city_id
-      @user_city = City.find(user_city_id).name
-
-
-
+  
       render :show
     else
       redirect_to root_path
